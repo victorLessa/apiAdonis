bbd77aaafa9690c6cdf41899350dfab6a9b307ce
victor <victor.lessa@acheifarmacias.com.br>
2019-04-19 10:55:11 -0300

    crud para tweets

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const Tweet = use('App/Models/Tweet')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const tweets = await Tweet.query().with('user').fetch()
+    return tweets
+  }
+
+  /**
+   * Create/save a new tweet.
+   * POST tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth }) {
+    const data = request.only(['content'])
+    // auth.user.id pega o id do usuario logado na aplicação
+    const tweet = await Tweet.create({ user_id: auth.user.id, ...data })
+    return tweet
+  }
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const tweet = await Tweet.findOrFail(params.id)
+    return tweet
+  }
+
+  /**
+   * Update tweet details.
+   * PUT or PATCH tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+
+  }
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, auth }) {
+    console.log(params.id)
+    const tweet = await Tweet.findOrFail(params.id)
+
+    if (tweet.user_id !== auth.user.id) {
+      return Response.status(401)
+    }
+
+    await tweet.delete()
+  }
+}
+
+module.exports = TweetController
--- start/routes.js
+++ start/routes.js
@@ -20,3 +20,7 @@ Route.get('/', 'AppController.index')
 Route.post('/register', 'AuthController.register')
 Route.post('/authenticate', 'AuthController.authenticate')
 Route.get('/app', 'AppController.index').middleware(['auth'])
+
+Route.group(() => {
+    Route.resource('tweets', 'TweetController').apiOnly().except('update')
+}).middleware('auth')
